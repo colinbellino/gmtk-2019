9de9fdd408396a02cf00a8c289d15d2c787a8d6c
Colin Bellino <bellino.colin@gmail.com>
2019-08-03 22:34:35 +0200

    ✨ Change units positions

--- Assets/Code/InitBattleState.cs
+++ Assets/Code/InitBattleState.cs
@@ -12,18 +12,18 @@ public class InitBattleState : IBattleState
 
 	public void EnterState()
 	{
-		for (int i = 0; i < _manager.Units.Length; i++)
-		{
-			_manager.Units[i].Facade = SpawnUnit(_manager.Units[i], i);
-		}
+		_manager.Units[0].Facade = SpawnUnit(_manager.Units[0], new Vector3(2f, 0f, 0f));
+		_manager.Units[1].Facade = SpawnUnit(_manager.Units[1], new Vector3(2f, 2f, 0f));
+		_manager.Units[2].Facade = SpawnUnit(_manager.Units[2], new Vector3(7f, 0f, 0f));
+		_manager.Units[3].Facade = SpawnUnit(_manager.Units[3], new Vector3(7f, 2f, 0f));
 
 		_manager.ChangeState(BattleStates.PlayerTurn);
 	}
 
-	private UnitFacade SpawnUnit(Unit unit, int index)
+	private UnitFacade SpawnUnit(Unit unit, Vector2 position)
 	{
 		var instance = GameObject.Instantiate(_manager.UnitPrefab);
-		instance.transform.position = new Vector3(3f * index, 0f, 0f);
+		instance.transform.position = position;
 		instance.name = unit.Name;
 
 		var facade = instance.GetComponent<UnitFacade>();

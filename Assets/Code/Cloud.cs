ce48c7799f91ae2da09a7ec724e9fbb79e5abd2b
Colin Bellino <bellino.colin@gmail.com>
2019-08-04 16:47:21 +0200

    🐛 Moving cloud and background fixes

---
+++ Assets/Code/Cloud.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+	[SerializeField] private Vector3 _movement = new Vector3(0.002f, 0f, 0f);
+
+	private void Update()
+	{
+		transform.position += _movement;
+	}
+
+	// private float _min;
+	// private float _max;
+
+	// private void Start()
+	// {
+	// 	_min = transform.position.y + 0f;
+	// 	_max = transform.position.y + 0.02f;
+	// }
+
+	// private void Update()
+	// {
+	// 	transform.position = new Vector3(
+	// 		transform.position.z,
+	// 		Mathf.PingPong(Time.time * 0.002f, _max - _min) + _min,
+	// 		transform.position.z
+	// 	);
+	// }
+}
